2017ca9aa92dc9231daf638df34832421dfb6028
Najla Shainan <najlanassersh@gmail.com>
2023-12-08 01:38:35 +0300

    fix more errors in excercise 2

--- things/views.py
+++ things/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_protect
 from things.forms import ThingForm
 
+@csrf_protect
 def home(request):
     form = ThingForm()
     return render(request, 'home.html', {'form': form})
