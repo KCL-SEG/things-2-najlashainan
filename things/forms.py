a8862e1a2562f4e9e5d9aa2afc2648fc14dc9a12
Najla Shainan <najlanassersh@gmail.com>
2023-12-08 01:29:12 +0300

    solve excercise 1 for Things 2

--- things/forms.py
+++ things/forms.py
@@ -1,3 +1,16 @@
 """Forms of the project."""
+from django import forms  
+from things.models import Thing  
 
 # Create your forms here.
+
+  
+class ThingForm(forms.ModelForm):  
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["description"].widget = forms.Textarea()
+        self.fields["quantty"].widget = forms.NumberInput()
+
+    class Meta:  
+        model = Thing  
+        exclude = ('created_at',)
